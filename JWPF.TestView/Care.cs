4e40051d5003fdc1938e3a6fc806a6d0c5bf0464
wangjianjun@ellabook.cn <wangjianjun@ellabook.cn>
2018-09-29 18:17:19 +0800

    w

---
+++ JWPF.TestView/Care.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWPF.TestView
+{
+    /// <summary>
+    /// <summary>
+    /// </summary>
+    public class Car
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 图片路径
+        /// </summary>
+        public string ImagePath { get; set; }
+        /// <summary>
+        /// 制造商
+        /// </summary>
+        public string Automarker { get; set; }
+        /// <summary>
+        /// 出产日期
+        /// </summary>
+        public string Year { get; set; }
+    }
+}
